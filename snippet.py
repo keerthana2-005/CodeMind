c9863e2e86f1f8198c2680bc9d4d5c0b4a49bf9b
SAI KEERTHANA THIMMAPPAGARI <153196340+keerthana2-005@users.noreply.github.com>
2025-02-25 10:32:59 +0530

    Update snippet.py

--- snippet.py
+++ snippet.py
@@ -1,1 +1,8 @@
-print(1+1)
+def find_max(lst):
+    max_val = 0  # Logical Error: Should initialize with lst[0] instead of 0
+    for num in lst:
+        if num > max_val:
+            max_val = num
+    return max_val
+
+print(find_max([-5, -2, -8, -1]))  # Incorrect output expected
